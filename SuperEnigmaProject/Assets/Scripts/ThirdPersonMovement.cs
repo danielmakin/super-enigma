02a77db33c1576786515bd28dc550a5984508296
danielmakin <daniel.makin10@gmail.com>
2023-06-07 13:43:38 +0100

    Player Moves
    Technically works but once a button is pressed, another button cannot be until the first is released and the move variable is reset

--- SuperEnigmaProject/Assets/Scripts/ThirdPersonMovement.cs
+++ SuperEnigmaProject/Assets/Scripts/ThirdPersonMovement.cs
@@ -6,12 +6,24 @@ using UnityEngine.InputSystem;
 public class ThirdPersonMovement : MonoBehaviour
 {   public CharacterController controller;
     public float speed = 6f;
+    bool cancelled;
+    Vector2 move;
     InputMaster controls;
     void Awake(){
         controls = new InputMaster();
 
-        controls.Player.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+        controls.Player.Movement.performed += ctx => {move = ctx.ReadValue<Vector2>();};//Move(ctx.ReadValue<Vector2>());
+        controls.Player.Movement.canceled += _ => {move = Vector2.zero;};
         controls.Player.Jump.performed += _ => Jump();
+    }
+
+    void Update(){
+        Vector3 direction = new Vector3(move.x, 0, move.y).normalized; //MEANS CANT MOVE FASTER WHEN DIAGONAL
+
+        if (direction.magnitude >= 0.1){
+            Debug.Log("MOve player to " + direction);
+            controller.Move(direction * speed * Time.deltaTime);
+        }
     }
 
     void Jump()
@@ -20,16 +32,7 @@ public class ThirdPersonMovement : MonoBehaviour
         Debug.Log("Player wants to JUMP...");
     }
 
-    void OnEnable(){
-        controls.Enable();
-    }
+    void OnEnable() => controls.Enable();
 
-    void OnDisable(){
-        controls.Disable();
-    }
-
-    void Move(Vector2 dir){
-        //MOVE THE PLAYER
-        Debug.Log("Player wants to MOVE TO : " + dir);
-    }
+    void OnDisable() => controls.Disable();
 }
